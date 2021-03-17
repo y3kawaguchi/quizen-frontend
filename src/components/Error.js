7fc98f5c5515dc26b8ef092d75109fe509e1eb09
y3kawaguchi <ykawaguchi@YusukenoMacBook-Pro.local>
2021-03-17 23:06:55 +0900

    Add routing

---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Error = () => {
+  return <h2>エラー画面</h2>;
+}
+
+export default Error;
---
+++ src/components/List.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const List = () => {
+  return <h2>一覧画面</h2>;
+}
+
+export default List;
